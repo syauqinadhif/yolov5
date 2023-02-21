e2177e37c04298c149c2a281a27ab1121082b078
syauqinadhif <76267893+syauqinadhif@users.noreply.github.com>
2023-02-21 20:41:42 +0700

    Update train.py

--- yolov5-train/train.py
+++ yolov5-train/train.py
@@ -49,7 +49,7 @@ from utils.general import (LOGGER, check_dataset, check_file, check_git_status, 
                            labels_to_class_weights, labels_to_image_weights, methods, one_cycle,
                            print_args, print_mutation, strip_optimizer)
 from utils.loggers import Loggers
-from object_detection.utils.loggers.wandb.wandb_utils import check_wandb_resume
+from utils.loggers.wandb.wandb_utils import check_wandb_resume
 from utils.loss import ComputeLoss
 from utils.metrics import fitness
 from utils.plots import plot_evolve, plot_labels
